38fd933e3d1ac63d9fd0942ad476a7afe8aade39
Matheus Rian <souzamatheusrian@gmail.com>
2022-01-15 19:36:08 -0300

    feat: Widget login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,56 +13,82 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset('assets/images/bulls.png')),
+                  SizedBox(height: 20),
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  SizedBox(height: 10),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'matheus@gmail.com' && password == '123') {
-                      print('Logado');
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Login não encontrado');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (email == 'matheus@gmail.com' && password == '123') {
+                        print('Logado');
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('Login não encontrado');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            'assets/images/matheus.HEIC',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
